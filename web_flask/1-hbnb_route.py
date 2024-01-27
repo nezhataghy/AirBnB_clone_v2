1423cd6c58a56c2715e2afe54c4777032e5e9b80
nezhataghy <nezhataghy@gmail.com>
2024-01-27 18:30:14 +0000

    HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This module starts a Flask web application"""""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Displays Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Displays HBNB"""
+    return "HBNB"
+
+
+if __name__ == '__main__':
+    """Listening on"""
+    app.run(port=5000, host='0.0.0.0')
