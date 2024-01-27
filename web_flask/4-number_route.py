0a82fb642dbc48bcb08a6effae132d0aaa7558b8
nezhataghy <nezhataghy@gmail.com>
2024-01-27 18:52:35 +0000

    C is fun!

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -20,8 +20,20 @@ def hbnb():
 
 @app.route("/c/<text>", strict_slashes=False)
 def handle_c_route(text):
-    """handling C route"""
+    """Handling C routes"""
     return f"C {escape(text.replace('_', ' '))}"
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def handle_python_route(text="is cool"):
+    """Handling Python route"""
+    return f"Python {escape(text.replace('_', ' '))}"
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def display_integer(number):
+    """"""
 
 
 if __name__ == '__main__':
