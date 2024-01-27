7c37a168abb82566a6ec954da147a1e713bf13c4
nezhataghy <nezhataghy@gmail.com>
2024-01-27 18:23:36 +0000

    Hello Flask!

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This module starts a Flask web application"""""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Displays Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+if __name__ == '__main__':
+    """Listening on"""
+    app.run(port=5000, host='0.0.0.0')
